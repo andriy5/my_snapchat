e03b594f30b3a24f7b045215ff845b288fd7b06f
Andriy <andriy.shakh@epitech.eu>
2020-07-14 16:39:08 +0200

    Updates

---
+++ precache-manifest.eb6c8ec13bae7fe111917ac7088c9925.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4e6a2ed43d554e696d52ab47afdddc",
+    "url": "/my_snapchat/index.html"
+  },
+  {
+    "revision": "a2e60755c4519cb90b20",
+    "url": "/my_snapchat/static/css/2.0955cf81.chunk.css"
+  },
+  {
+    "revision": "d738798becf741d624ac",
+    "url": "/my_snapchat/static/css/main.6942c781.chunk.css"
+  },
+  {
+    "revision": "a2e60755c4519cb90b20",
+    "url": "/my_snapchat/static/js/2.880b7118.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_snapchat/static/js/2.880b7118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d738798becf741d624ac",
+    "url": "/my_snapchat/static/js/main.549aa711.chunk.js"
+  },
+  {
+    "revision": "fa1fed93fcc6570da9a7",
+    "url": "/my_snapchat/static/js/runtime-main.0119c3c9.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/my_snapchat/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/my_snapchat/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/my_snapchat/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/my_snapchat/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "54bea9c34ea287e05ee437d6def03e82",
+    "url": "/my_snapchat/static/media/ghost512.54bea9c3.png"
+  }
+]);
